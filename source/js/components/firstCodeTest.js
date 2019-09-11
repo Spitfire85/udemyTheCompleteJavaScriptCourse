771e3b5eab7be5d78f50440dbb706c7bed3a44aa
Matt Clark <m.clark@ry.com>
2019-09-11 12:44:14 +0100

    First code tweak mk2

--- source/js/components/firstCodeTest.js
+++ source/js/components/firstCodeTest.js
@@ -2,14 +2,17 @@ const firstCodeTest = {
   init() {
     // console.log('firstCodeTest');
 
-    let johnHeight = 189;
+    let johnHeight = 1.89;
     let johnMass = 81;
 
-    let markHeight = 174;
+    let markHeight = 1.74;
     let markMass = 74;
 
     let johnBmi = johnMass / (johnHeight * johnHeight);
     let markBmi = markMass / (markHeight * markHeight);
+
+    console.log("John's BMI: " + johnBmi);
+    console.log("Mark's BMI: " + markBmi);
 
     let bmiResult = markBmi > johnBmi;
 
