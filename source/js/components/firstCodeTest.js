374119aaaec3f895ab5fc708321bfdb2851a004f
Matt Clark <m.clark@ry.com>
2019-09-11 12:27:13 +0100

    Setting up separate JS files

---
+++ source/js/components/firstCodeTest.js
@@ -0,0 +1,16 @@
+const firstCodeTest = {
+  init() {
+    console.log('firstCodeTest');
+
+    let johnHeight = 189;
+    let johnMass = 81;
+
+    let markHeight = 174;
+    let markMass = 74;
+
+    let johnBMI = johnMass / (johnHeight * johnHeight);
+    let markBMI = markMass / (markHeight * markHeight);
+  }
+};
+
+export default firstCodeTest;
--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,1 +1,12 @@
-console.log('hello');
+import firstCodeTest from './components/firstCodeTest';
+
+const jsCourse = {
+  init() {
+    const root = document.documentElement;
+    root.classList.remove('no-js');
+    root.classList.add('js');
+    firstCodeTest.init();
+  }
+};
+
+jsCourse.init();
