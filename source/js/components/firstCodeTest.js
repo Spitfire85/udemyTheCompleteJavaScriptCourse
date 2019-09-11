f52e4e2f767ca6f9f35d24ea17163b47316f646a
Matt Clark <m.clark@ry.com>
2019-09-11 15:52:29 +0100

    Boolean logic work

--- source/js/components/firstCodeTest.js
+++ source/js/components/firstCodeTest.js
@@ -16,7 +16,30 @@ const firstCodeTest = {
 
     let bmiResult = markBmi > johnBmi;
 
-    console.log("Mark's BMI is higher than John's: " + bmiResult);
+    if (markBmi > johnBmi) {
+      console.log("Mark's BMI is higher than John's: " + bmiResult);
+    } else {
+      console.log("Mark's BMI is lower than John's: " + bmiResult);
+    }
+
+    // console.log("Mark's BMI is higher than John's: " + bmiResult);
+
+    let firstName = 'John';
+    let civilStatus = 'single';
+
+    if (civilStatus === 'married') {
+      console.log(firstName + ' is married');
+    } else {
+      console.log(firstName + ' is not married');
+    }
+
+    let isMarried = false;
+
+    if (isMarried) {
+      console.log(firstName + ' is married');
+    } else {
+      console.log(firstName + ' is not married');
+    }
   }
 };
 
--- source/js/scripts.js
+++ source/js/scripts.js
@@ -5,7 +5,20 @@ const jsCourse = {
     const root = document.documentElement;
     root.classList.remove('no-js');
     root.classList.add('js');
-    firstCodeTest.init();
+    // firstCodeTest.init();
+
+    let firstName = 'John';
+    let age = 34;
+
+    if (age < 13) {
+      console.log(firstName + ' is a boy');
+    } else if (age >= 13 && age < 20) {
+      console.log(firstName + ' is a teenager');
+    } else if (age >= 20 && age < 30) {
+      console.log(firstName + ' is a young man');
+    } else {
+      console.log(firstName + ' is a man');
+    }
   }
 };
 
