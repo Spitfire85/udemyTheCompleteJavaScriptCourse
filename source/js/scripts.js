5a46ca2556646d638d06eb62e743a54e1fc74c82
MattClark48 <mattclark48@hotmail.com>
2019-10-01 21:50:11 +0100

    Updated notes and theory

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -13,7 +13,7 @@ const jsCourse = {
     // secondCodeTest.init();
     // thirdCodeTest.init();
     // forthCodeTest.init();
-    fifthCodeTest.init();
+    // fifthCodeTest.init();
 
     // Fizz buss
     // var i = 1;
@@ -247,6 +247,200 @@ const jsCourse = {
     //   console.log(john[i]);
     //   i++;
     // }
+
+    /////////////////////////////////////
+    // Lecture: Hoisting
+
+    //functions
+    // calculateAge(1990);
+
+    // function calculateAge(year) {
+    //   console.log(2016 - year);
+    // }
+
+    // var calculateAge2 = function(year) {
+    //   console.log(2016 - year);
+    // };
+
+    // calculateAge2(1990);
+
+    // console.log(age);
+    // //variables
+    // var age = 23;
+    // console.log(age);
+
+    // function foo() {
+    //   var age = 12;
+    //   console.log(age);
+    // }
+    // foo();
+    // console.log(age);
+
+    /*
+    // functions
+    calculateAge(1965);
+
+    function calculateAge(year) {
+        console.log(2016 - year);
+    }
+
+    // retirement(1956);
+    var retirement = function(year) {
+        console.log(65 - (2016 - year));
+    }
+
+
+    // variables
+
+    console.log(age);
+    var age = 23;
+
+    function foo() {
+        console.log(age);
+        var age = 65;
+        console.log(age);
+    }
+    foo();
+    console.log(age);
+    */
+
+    /////////////////////////////////////
+    // Lecture: Scoping
+
+    /*
+    // First scoping example
+    var a = 'Hello!';
+    first();
+
+    function first() {
+        var b = 'Hi!';
+        second();
+
+        function second() {
+            var c = 'Hey!';
+            console.log(a + b + c);
+        }
+    }
+
+
+    // Example to show the differece between execution stack and scope chain
+    var a = 'Hello!';
+    first();
+
+    function first() {
+        var b = 'Hi!';
+        second();
+
+        function second() {
+            var c = 'Hey!';
+            third()
+        }
+    }
+
+    function third() {
+        var d = 'John';
+        //console.log(c);
+        console.log(a+d);
+    }
+    */
+    // var a = 'Hello!';
+    // first();
+
+    // function first() {
+    //   var b = 'Hi!';
+    //   second();
+
+    //   function second() {
+    //     var c = 'Hey!';
+    //     third();
+    //   }
+    // }
+
+    // function third() {
+    //   var d = 'John';
+    //   //console.log(c);
+    //   console.log(a + d);
+    // }
+    // console.log(this);
+
+    function calculateAge(year) {
+      console.log(2016 - year);
+      console.log(this);
+    }
+
+    var john = {
+      name: 'John',
+      yearOfBirth: 1990,
+      calculateAge: function() {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
+
+        function innerFunction() {
+          console.log(this);
+        }
+        innerFunction();
+      }
+    };
+
+    john.calculateAge(23);
+
+    var mike = {
+      name: 'Mike',
+      yearOfBirth: 1984
+    };
+
+    //METHOD BORROWING
+    mike.calculateAge = john.calculateAge;
+    mike.calculateAge(22);
+
+    // var ageObj = {
+    //   age: 12,
+    //   calculateAge: function(year) {
+    //     console.log(2016 - year);
+    //     console.log(this);
+    //   }
+    // };
+
+    // ageObj.calculateAge(1985);
+
+    /////////////////////////////////////
+    // Lecture: The this keyword
+
+    /*
+    //console.log(this);
+
+    calculateAge(1985);
+
+    function calculateAge(year) {
+        console.log(2016 - year);
+        console.log(this);
+    }
+
+    var john = {
+        name: 'John',
+        yearOfBirth: 1990,
+        calculateAge: function() {
+            console.log(this);
+            console.log(2016 - this.yearOfBirth);
+            
+            function innerFunction() {
+                console.log(this);
+            }
+            innerFunction();
+        }
+    }
+
+    john.calculateAge();
+
+    var mike = {
+        name: 'Mike',
+        yearOfBirth: 1984
+    };
+
+
+    mike.calculateAge = john.calculateAge;
+    mike.calculateAge();
+    */
   }
 };
 
