9b5f2bd23ef4309b940ff887fd5ac71a196f536b
MattClark48 <mattclark48@hotmail.com>
2020-04-22 16:17:07 +0100

    Start building search

--- sections/forkify/README.md
+++ sections/forkify/README.md
@@ -128,3 +128,5 @@ https://developer.mozilla.org/en-US/docs/Web/API
 Array methods explained
 https://codeburst.io/array-methods-explained-filter-vs-map-vs-reduce-vs-foreach-ea3127c6d319
 
+Fetch or Axios
+https://blog.logrocket.com/axios-or-fetch-api/
--- sections/forkify/src/js/index.js
+++ sections/forkify/src/js/index.js
@@ -1,4 +1,36 @@
 // Global app controller
-import x from './test';
+import Search from './models/Search';
 
-console.log(`I imported ${x} from another module`);
+//Global state 
+//- Search object
+//- Current recipe
+//- Shopping list object
+//- Liked recipes
+
+const state = {};
+
+const controlSearch = async () => {
+  console.log('controlSearch');
+  // 1) Get query from view
+  const query = 'pizza'; // Update later
+  if (query) {
+    // 2) New search object and add to state
+    state.search = new Search(query);
+  }
+
+  // 3) Prepare UI for results
+
+  // 4) Search for recipe
+  await state.search.getResults();
+
+  // 5) Render results on UI
+  console.log(state.search.result);
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
+
+const search = new Search('pizza');
+
