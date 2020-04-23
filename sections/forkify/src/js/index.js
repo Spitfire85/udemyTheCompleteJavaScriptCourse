c68d9b59ac4b46d7fb6ab7e1847a18337de56604
MattClark48 <mattclark48@hotmail.com>
2020-04-23 10:02:54 +0100

    Updated search

--- sections/forkify/src/js/index.js
+++ sections/forkify/src/js/index.js
@@ -1,5 +1,7 @@
 // Global app controller
 import Search from './models/Search';
+import * as searchView from './views/searchView'
+import { elements, renderResults, renderLoader, clearLoader } from './views/base'
 
 //Global state 
 //- Search object
@@ -10,27 +12,29 @@ import Search from './models/Search';
 const state = {};
 
 const controlSearch = async () => {
-  console.log('controlSearch');
   // 1) Get query from view
-  const query = 'pizza'; // Update later
-  if (query) {
-    // 2) New search object and add to state
+  const query = searchView.getInput(); // Update later
+
+  // 2) New search object and add to state
+  if (query) {    
     state.search = new Search(query);
   }
 
   // 3) Prepare UI for results
+  searchView.clearInput();
+  searchView.clearResults();
+  renderLoader(elements.searcRes);
 
   // 4) Search for recipe
   await state.search.getResults();
 
   // 5) Render results on UI
-  console.log(state.search.result);
+  clearLoader();
+  searchView.renderResults(state.search.result);
 };
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 });
-
-const search = new Search('pizza');
 
