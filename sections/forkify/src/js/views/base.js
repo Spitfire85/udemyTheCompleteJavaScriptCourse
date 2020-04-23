abad7cfc0d1ebf0ed43e5e1dad7730f32bebe3ac
MattClark48 <mattclark48@hotmail.com>
2020-04-23 11:52:05 +0100

    Pagination fixed

--- sections/forkify/src/js/views/base.js
+++ sections/forkify/src/js/views/base.js
@@ -2,7 +2,8 @@ export const elements = {
   searchForm: document.querySelector('.search'),
   searchInput: document.querySelector('.search__field'),
   searchResList: document.querySelector('.results__list'),
-  searcRes: document.querySelector('.results')
+  searcRes: document.querySelector('.results'),
+  searchResPages: document.querySelector('.results__pages')
 };
 
 export const elementStrings = {
